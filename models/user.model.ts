f3f1419aecbb7ec0e0c8fc63bf122c7997ee1b6b
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-03-05 10:18:59 +0000

    added product controllers

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,78 +1,45 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); // If including password hashing
+// Define interfaces for your data structures
+interface IUser extends Document {
+    username: string;
+    email: string;
+    password?: string; // Optional for alternative auth methods
+    webauthnCredentials: Array<object>; // Replace 'Object' with a suitable interface if you have one
+    totpSecret?: string;
+    products: Array<mongoose.Schema.Types.ObjectId>; // Array of Product IDs
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     // ... other fields
-});
+}
 
-// Example of a pre-save hook for password hashing
-userSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-});
-
-module.exports = mongoose.model('User', userSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUser>({ // Use our interface
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String }, // If traditional auth is used
-    webauthnCredentials: [Object], // Schema for WebAuthn public keys
+    password: { type: String },
+    webauthnCredentials: [Object],
     totpSecret: { type: String },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+
     // ... other fields
 });
 
-// Hash password before saving (if using traditional auth)
-userSchema.pre('save', async function(next) {
+// Hash password (if using traditional auth)
+userSchema.pre('save', async function(this: IUser, next) { // Adjust 'this' type
     if (this.password && this.isModified('password')) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model<IUser>('User', userSchema);
+
+
+
+
+
+
+
+
+
+
