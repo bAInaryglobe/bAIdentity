cfb63d1380eb44de3cbeba2a5a695ceb478a9813
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-03-19 15:29:14 +0100

    separate or merge routes?

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,1 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/routes/user.routes.ts
+const express_1 = __importDefault(require("express"));
+const session_middleware_1 = __importDefault(require("../middleware/session.middleware"));
+const router = express_1.default.Router();
+// Routes that require session validation
+router.get('/profile', session_middleware_1.default, (req, res) => {
+    // Access session data: req.session
+    // ... your route logic
+});
+router.post('/update-settings', session_middleware_1.default, (req, res) => {
+    // ...
+});
+// Routes that don't require session validation (if any)
+router.get('/about', (req, res) => {
+    // ...
+});
+exports.default = router;
